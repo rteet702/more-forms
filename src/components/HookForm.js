2e7a004373c1d22c695ca02135165d02d12ca2df
Robert Teets <teetsrobert@gmail.com>
2022-09-26 15:56:13 -0600

    2: assignment tasks complete

--- src/components/HookForm.js
+++ src/components/HookForm.js
@@ -3,52 +3,132 @@ import { useState } from 'react'
 const HookForm = (props) => {
 
     const [firstName, setFirstName] = useState("")
+    const [firstNameError, setFirstNameError] = useState("")
+
     const [lastName, setLastName] = useState("")
+    const [lastNameError, setLastNameError] = useState("")
+
     const [email, setEmail] = useState("")
+    const [emailError, setEmailError] = useState("")
+
     const [password, setPassword] = useState("")
+    const [passwordError, setPasswordError] = useState("")
+
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [confirmPasswordError, setConfirmPasswordError] = useState("")
+
+    // first name validation
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value)
+
+        if ( e.target.value.length < 1 || e.target.value.length > 2 ) {
+            setFirstNameError('')
+        }
+        else {
+            setFirstNameError('First Name must be at least 3 characters long.')
+        }
+    }
+
+    // last name validation
+    const handleLastName = (e) => {
+        setLastName(e.target.value)
+
+        if ( e.target.value.length < 1 || e.target.value.length > 2 ) {
+            setLastNameError('')
+        }
+        else {
+            setLastNameError('Last Name must be at least 3 characters long.')
+        }
+    }
+
+    // email validation
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+
+        if ( e.target.value.length < 1 || e.target.value.length > 5) {
+            setEmailError('')
+        }
+        else {
+            setEmailError('Email must be at least 5 characters long.')
+        }
+    }
+
+    // password validation
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+
+        if ( e.target.value.length < 1 || e.target.value.length > 7 ) {
+            setPasswordError('')
+        }
+        else {
+            setPasswordError('Password must be at least be 8 characters long.')
+        }
+    }
+
+    // password confirmation validation
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value)
+
+        console.log(password, confirmPassword)
+
+        if ( password === e.target.value ) {
+            setConfirmPasswordError('')
+        }
+        else {
+            setConfirmPasswordError('Passwords must match.')
+        }
+    }
 
     return (
         <>
           <form>
+
             <div>
                 <label htmlFor='firstName'>First Name:</label>
-                <input type='text' id='firstName' onChange={ (e) => setFirstName(e.target.value)}/>
+                <input type='text' id='firstName' onChange={ handleFirstName }/>
+                {
+                    firstNameError ?
+                    <p>{ firstNameError }</p> : undefined
+                }
             </div>
+
             <div>
                 <label htmlFor='lastName'>Last Name:</label>
-                <input type='text' id='lastName' onChange={ (e) => setLastName(e.target.value)}/>
+                <input type='text' id='lastName' onChange={ handleLastName }/>
+                {
+                    lastNameError ?
+                    <p>{ lastNameError }</p> : undefined
+                }
             </div>
+
             <div>
                 <label htmlFor='email'>Email Address:</label>
-                <input type='email' id='email' onChange={ (e) => setEmail(e.target.value)}/>
+                <input type='email' id='email' onChange={ handleEmail }/>
+                {
+                    emailError ?
+                    <p>{ emailError }</p> : undefined
+                }
             </div>
+
             <div>
                 <label htmlFor='password'>Password:</label>
-                <input type='password' id='password' onChange={ (e) => setPassword(e.target.value)}/>
+                <input type='password' id='password' onChange={ handlePassword }/>
+                {
+                    passwordError ?
+                    <p>{ passwordError }</p> : undefined
+                }
             </div>
+
             <div>
                 <label htmlFor='confirmPassword'>Confirm Password:</label>
-                <input type='password' id='confirmPassword' onChange={ (e) => setConfirmPassword(e.target.value)}/>
+                <input type='password' id='confirmPassword' onChange={ handleConfirmPassword }/>
+                {
+                    confirmPasswordError ?
+                    <p>{ confirmPasswordError }</p> : undefined
+                }
             </div>
-          </form>
 
-          <div className='row'>
-            <div className='col test'>
-                <p>First Name:</p>
-                <p>Last Name:</p>
-                <p>Email:</p>
-                <p>Password:</p>
-                <p>Confirm Password:</p>
-            </div>
-            <div className='col'>
-                <p>{ firstName }</p>
-                <p>{ lastName }</p>
-                <p>{ email }</p>
-                <p>{ password }</p>
-                <p>{ confirmPassword }</p>
-            </div>
-          </div>
+          </form>
         </>
     )
 }
